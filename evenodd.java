75a10ddec0048b4a031a151d178d82843d8760ab
karankambli25 <kashidakshay989@gmail.com>
2025-07-12 22:19:25 +0530

    even odd code added

---
+++ evenodd.java
@@ -0,0 +1,11 @@
+class EvenOdd{
+    public static void main(String a[]){
+        int num = 4;
+        if (num%2==0){
+            System.out.println("This is even number");
+        }
+        else{
+            System.out.println("This is odd number");
+        }
+    }
+}
